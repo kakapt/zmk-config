82ad2c4e854ea7eaaab0ab4e2815dd7d4dee3b39
Thinh Vu <phuocthinhvu@gmail.com>
2026-06-05 16:54:40 +0700

    refactor settings_reset package

--- flake.nix
+++ flake.nix
@@ -16,86 +16,63 @@
     }:
     let
       forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+
+      firmwareArgs = {
+        name = "firmware";
+
+        src = nixpkgs.lib.sourceFilesBySuffices self [
+          ".board"
+          ".cmake"
+          ".conf"
+          ".defconfig"
+          ".dts"
+          ".dtsi"
+          ".json"
+          ".keymap"
+          ".overlay"
+          ".shield"
+          ".yml"
+          "_defconfig"
+        ];
+
+        board = "nice_nano_v2";
+        shield = "sofle_%PART%";
+
+        zephyrDepsHash = "sha256-F03oJNHWmHlpFc1JHyvqX02WL+Pg6ZcNWpCaiDfJANA=";
+
+        snippets = [
+          "nrf52840-nosd"
+        ];
+
+        config = "./config";
+
+        enableZmkStudio = true;
+
+        meta = {
+          description = "ZMK firmware";
+          license = nixpkgs.lib.licenses.mit;
+          platforms = nixpkgs.lib.platforms.all;
+        };
+      };
+
     in
     {
       packages = forAllSystems (system: rec {
         default = firmware;
 
-        firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
-          name = "firmware";
+        firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard firmwareArgs;
 
-          src = nixpkgs.lib.sourceFilesBySuffices self [
-            ".board"
-            ".cmake"
-            ".conf"
-            ".defconfig"
-            ".dts"
-            ".dtsi"
-            ".json"
-            ".keymap"
-            ".overlay"
-            ".shield"
-            ".yml"
-            "_defconfig"
-          ];
-
-          board = "nice_nano_v2";
-          shield = "sofle_%PART%";
-
-          zephyrDepsHash = "sha256-F03oJNHWmHlpFc1JHyvqX02WL+Pg6ZcNWpCaiDfJANA=";
-
-          snippets = [
-            "nrf52840-nosd"
-          ];
-
-          config = "./config";
-
-          enableZmkStudio = true;
-
-          meta = {
-            description = "ZMK firmware";
-            license = nixpkgs.lib.licenses.mit;
-            platforms = nixpkgs.lib.platforms.all;
-          };
-        };
-
-        settings_reset = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
-          name = "settings_reset";
-
-          src = nixpkgs.lib.sourceFilesBySuffices self [
-            ".board"
-            ".cmake"
-            ".conf"
-            ".defconfig"
-            ".dts"
-            ".dtsi"
-            ".json"
-            ".keymap"
-            ".overlay"
-            ".shield"
-            ".yml"
-            "_defconfig"
-          ];
-
-          board = "nice_nano_v2";
-          shield = "settings_reset";
-
-          zephyrDepsHash = "sha256-F03oJNHWmHlpFc1JHyvqX02WL+Pg6ZcNWpCaiDfJANA=";
-
-          snippets = [
-            "nrf52840-nosd"
-          ];
-
-          parts = [ "reset" ];
-
-          config = "./config";
-
-          meta = {
-            description = "ZMK firmware";
-            license = nixpkgs.lib.licenses.mit;
-            platforms = nixpkgs.lib.platforms.all;
-          };
-        };
+        settings_reset = zmk-nix.legacyPackages.${system}.buildSplitKeyboard (
+          firmwareArgs
+          // {
+            enableZmkStudio = false;
+            name = "settings_reset";
+            shield = "settings_reset";
+            parts = [
+              "reset"
+            ];
+          }
+        );
 
         flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
         update = zmk-nix.packages.${system}.update;
